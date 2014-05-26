82e10c583028ad5bf531c574e63ead9af5b915ed
Peter Varga <petervarga@rarecrew.com>
2014-05-26 11:44:22 +0200

    Temp commit

---
+++ Application/ClubArcada/ClubArcada.Win/Dialogs/NewTableDlg.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClubArcada.Win.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for NewTableDlg.xaml
+    /// </summary>
+    public partial class NewTableDlg : Window
+    {
+        public NewTableDlg()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
